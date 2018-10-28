64656cbd649d165b7fc9c86d247fb3388a63d55b
Michael Nguyen <michaelnguyen@Michaels-MBP.home>
2018-10-28 12:54:35 -0400

    Improved form validation checking for front end form

--- web/js/validate.js
+++ web/js/validate.js
@@ -1,6 +1,29 @@
-var $myInput = $('#venueName').on('change', anotherFunction);
+$('#venueName').on('input',function(e){
+  var venueName = document.getElementById("venueName");
 
-var anotherFunction = function() {
-  $myInput.val('Another value');
-  $myInput.trigger('change');
-};
+  // Init a timeout variable to be used below
+  var timeout = null;
+
+  // Listen for keystroke events
+  venueName.onkeyup = function (e) {
+
+      // Clear the timeout if it has already been set.
+      // This will prevent the previous task from executing
+      // if it has been less than <MILLISECONDS>
+      clearTimeout(timeout);
+
+      // Make a new timeout set to go off in 800ms
+      timeout = setTimeout(function () { 
+        $("#venueName").attr("class", "form-control is-valid");
+      }, 500);
+  };
+
+});
+
+$('#startDate').on('input',function(e){
+  $("#startDate").attr("class", "form-control is-valid");
+});
+
+$('#endDate').on('input',function(e){
+  $("#endDate").attr("class", "form-control is-valid");
+});
