5e980644e3579a0ce389435191b26deb97638358
Michael Nguyen <michael102396@gmail.com>
2019-05-05 15:14:34 -0400

    Updating index

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const rp = require("request-promise");
-var events = require("./web/js/songkick_requests");
+// var events = require("./web/js/songkick_requests");
 
 const apiKey = "6hVA2yrccxVrs88S";
 var artistID = "8561929";
-var venue = "The Anthem";
+// var venue = "Merriweather Post Pavilion";
+var venue = "Echostage";
 
 var url = `https://api.songkick.com/api/3.0/artists/${artistID}/calendar.json?apikey=${apiKey}`;
 
@@ -17,16 +18,32 @@ var options = {
     },
 }
 
-// rp(options).then(data => {
-//     var json = JSON.parse(data);
-//     var venueList = json.resultsPage.results.venue;
-//     for (i = 0; i < venueList.length; i++) {
-//         console.log(venueList[i]);
-//     }
-// }).catch(err => {
-//     console.log(err);
-// });
+rp(options).then(data => {
+    var json = JSON.parse(data);
+    console.log(json);
+    var venueID = json.resultsPage.id;
+    var startDate = "2018-10-31";
+    var endDate = "2018-11-25";
 
-events.getVenueID(venue, apiKey).then(data => {
-    events.searchEventsAtVenue(data, apiKey, "2018-10-29", "2018-11-30");
-}).catch(error => console.log(error));
+    var venueEventsURL = `https://api.songkick.com/api/3.0/venues/${venueID}/calendar.json?apikey=${apiKey}&min_date=${startDate}&max_date=${endDate}`;
+
+    var newOption = {
+        uri: venueEventsURL,
+        headers: {
+            'User-Agent': 'Mues',
+            'Accept': 'application/json'
+        },
+    }
+
+    rp(newOption).then(data => {
+        var json = JSON.parse(data);
+
+        console.log(json.resultsPage.results);
+    })
+}).catch(err => {
+    console.log(err);
+});
+
+// events.getVenueID(venue, apiKey).then(data => {
+//     events.searchEventsAtVenue(data, apiKey, "2018-10-29", "2018-11-30");
+// }).catch(error => console.log(error));
